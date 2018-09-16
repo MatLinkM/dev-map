5cf2dd988f8701ccd6e1934a0ab9129e1f8baa5f
Matheus Medeiros <matheuslinkm@gmail.com>
2018-09-16 04:02:30 -0300

    install styled-components and configure file to global styles

--- dev-map/src/index.js
+++ dev-map/src/index.js
@@ -3,5 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
+import "./styles/global";
+
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
---
+++ dev-map/src/styles/global.js
@@ -0,0 +1,15 @@
+import { injectGlobal } from "styled-components";
+
+injectGlobal`
+ * {
+   margin: 0;
+   padding: 0;
+   box-sizing: border-box;
+   outline: 0;
+ }
+
+ body {
+   width: 100%;
+   height: 100%;
+ }
+`;
