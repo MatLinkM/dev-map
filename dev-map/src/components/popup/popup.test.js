40142e5b9356cca4b3a1e291d7087fd4a9fc122d
Matheus Medeiros <matheuslinkm@gmail.com>
2018-10-04 01:19:33 -0300

    excludes import unnecessary

--- dev-map/src/components/popup/popup.test.js
+++ dev-map/src/components/popup/popup.test.js
@@ -1,13 +1,9 @@
 import React from "react";
 import { shallow } from "enzyme";
 
-import Main from "../../page/main";
 import Popup from "./index";
 
 import store from "../../store";
-import { SaveButton, CancelButton } from "./styles";
-
-// const visiblePopup = true;
 
 describe("<Popup />", () => {
   it("visiblePopup should be true", () => {
